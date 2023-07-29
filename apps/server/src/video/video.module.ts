40eec933c11633f4d39d0ce33fc1e6ef1dbe2ea4
= <rohitbhatwara1@gmail.com>
2023-07-29 10:29:13 +0530

    🛠️ fix(video): add NotificationModule in video module dependency

--- apps/server/src/video/video.module.ts
+++ apps/server/src/video/video.module.ts
@@ -4,9 +4,10 @@ import { VideoService } from './video.service'
 import { FileModule } from '~/file/file.module'
 import { ChannelModule } from '~/channel/channel.module'
 import { PlaylistModule } from '~/playlist/playlist.module'
+import { NotificationModule } from '~/notification/notification.module'
 
 @Module({
-  imports: [FileModule, ChannelModule, PlaylistModule],
+  imports: [FileModule, ChannelModule, PlaylistModule, NotificationModule],
   controllers: [VideoController],
   providers: [VideoService],
   exports: [VideoService],
